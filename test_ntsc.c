20d702b196fe5a891e42c1b89c97737406864181
pa-art <pa-art@pa2.so-net.ne.jp>
2021-03-08 19:15:41 +0900

    Generation of holizontal sync signal is changed to use PWM interrupt.

--- test_ntsc.c
+++ test_ntsc.c
@@ -6,18 +6,18 @@
 #include <stdio.h>
 #include <string.h>
 #include "pico/stdlib.h"
-#include "pico/binary_info.h"
+#include "hardware/irq.h"
+#include "hardware/pwm.h"
 #include "hardware/adc.h"
-//#include "ascii_chars.h"
 #include "font8x8_basic.h"
 
 #define CONV_FACTOR (3.3f / (1 << 12)) // ADC data -> voltage (white Pico)
-//#define CONV_FACTOR (3.25f / (1 << 12)) // ADC data -> voltage (red Pico)
 #define ADC_TEMP    4 // temperature sensor input
 #define LED     25      // GPIO connected LED on the board
 #define MLED    (1 << LED)
 #define LEDON   gpio_put_masked(MLED, MLED)
 #define LEDOFF  gpio_put_masked(MLED, 0)
+#define GPPWM   2       // GPIO2 is PWM output
 #define GP14    14      // GPIO14 connected to RCA+ pin via 330 ohm
 #define GP15    15      // GPIO15 connected to RCA+ pin via 1k ohm
 #define M14     (1 << GP14) // bit mask for GPIO14
@@ -28,18 +28,18 @@
 #define GRAY    gpio_put_masked((M14 | M15), M14)           // GPIO14='H' and GPIO15='L'
 #define VRAM_W  20      // width size of VRAM
 #define VRAM_H  20      // height size of VRAM
-#define V_BASE  48      // horizontal line number to start displaying VRAM
+#define V_BASE  40      // horizontal line number to start displaying VRAM
+#define BAR_MAX 20      // bar length
 
-unsigned char vram[VRAM_W][VRAM_H]; // VRAM
+volatile unsigned char vram[VRAM_W][VRAM_H]; // VRAM
 int count = 1;                      // horizontal line counter
 int count_vsync = 0;
 int bx = 0; int by = 0;
 int bbx = 0; int bby = 0;
 int buf_len = 1;
-bool state = true;
+volatile bool state = true;
 int bar_len = 0;
 bool bar_inc = true;
-#define BAR_MAX 16
 
 // to clear VRAM contents (set to 0)
 void vram_clear( void ) {
@@ -141,46 +141,85 @@ void display_message_at_random_place( char *buf ) {
 // to generate horizontal sync siganl
 void hsync( void ) {
     SYNC;
-    //sleep_us(5);
     sleep_us(5);
     BLACK;
-    //sleep_us(7);
     sleep_us(7);
 }
 
 // to generate vertical sync siganl
 void vsync ( void ) {
     SYNC;
-    if (count_vsync % 10 == 0) {
-        // display bar
-        display_bar(16, '#');
-    }
-    if (count_vsync % 1000 == 0) {
-        // measure temprature and display it
-        measure_temp(13);
-    }
-    if (count_vsync % 200 == 0) {
-        // flip LED
-        flip_led();
-    }
-    if (count_vsync % 500 == 0) {
-        // display Hello! at random place
-        //display_message_at_random_place("Hello!");
-    }
-    //sleep_us(25);
-    sleep_us(25);
+    sleep_us(10);
+    sleep_us(5);
+    sleep_us(10);
     BLACK;
     sleep_us(5);
+
     SYNC;
-    //sleep_us(25);
-    sleep_us(25);
+    sleep_us(10);
+    sleep_us(5);
+    sleep_us(10);
     BLACK;
     sleep_us(5);
+
     count_vsync++;
 }
 
+// handler for holizontal line processing
+void horizontal_line( ) {
+    // Clear the interrupt flag that brought us here
+    pwm_clear_irq(pwm_gpio_to_slice_num(GPPWM));
+
+    // vertical synchronization duration
+    if (count >= 3 && count <= 5) {
+        vsync();    // vertical SYNC
+
+    // VRAM drawing area
+    } else if (count >= V_BASE && count < V_BASE + VRAM_H * CHAR_H) {
+        hsync();
+        // left blank??
+        BLACK;
+        sleep_us(0);    // should be tuned
+
+        // calculate VRAM y position from scan line number
+        int y = (count - V_BASE) / CHAR_H;
+        // horizontal VRAM drawing
+        for (int x = 0; x < VRAM_W; x++) {
+            // read VRAM
+            volatile unsigned char c = vram[x][y];
+            // draw bit from character table (ascii_table)
+            for (int w = 0; w < CHAR_W; w++) {
+                if ((ascii_table[c][count % CHAR_H] & (1 << w)) != 0) {
+                    GRAY; WHITE;
+                } else {
+                    BLACK; BLACK;
+                }
+            }
+        }
+
+/*
+        WHITE;
+        sleep_us(10);
+        sleep_us(10);
+*/
+        // right blank??
+        BLACK;
+    } else {
+        hsync();
+        BLACK;
+    }
+    // count up scan line 
+    count++;
+    // if scan line reach to max
+    if (count > 262) {
+        count = 1;
+    }
+    return;
+}
+
 int main() {
-    // initialize GPIO14 and GPIO15
+
+    // initialize GPIO14 and GPIO15 for masked output
     gpio_init(GP14);
     gpio_init(GP15);
     gpio_init_mask(M14 | M15);
@@ -200,22 +239,26 @@ int main() {
     adc_select_input(ADC_TEMP);    // ADC selected
     // clear VRAM
     vram_clear();
-/*
-    int cnt = 0;
-    for (int y = 0; y < VRAM_H; y ++) {
-        for (int x = 0; x < VRAM_W; x++) {
-            vram_write(x, y, cnt++);
-            if (cnt > ASCII_CHAR_NUM) {
-                cnt = 0;
-            }
-        }
-    }
-*/
-/*
-    for (int i = 0; i < 5; i++) {
-        vram_strings(i, 5+2*i, "Hello, world!");
-    }
-*/
+
+    // GPPWM pin is the PWM output
+    gpio_set_function(GPPWM, GPIO_FUNC_PWM);
+    // Figure out which slice we just connected to the GPPWM pin
+    uint slice_num = pwm_gpio_to_slice_num(GPPWM);
+
+    // Mask our slice's IRQ output into the PWM block's single interrupt line,
+    // and register our interrupt handler
+    pwm_clear_irq(slice_num);
+    pwm_set_irq_enabled(slice_num, true);
+    irq_set_exclusive_handler(PWM_IRQ_WRAP, horizontal_line);
+    irq_set_enabled(PWM_IRQ_WRAP, true); 
+
+    // Get some sensible defaults for the slice configuration. By default, the
+    // counter is allowed to wrap over its maximum range (0 to 2**16-1)
+    pwm_config config = pwm_get_default_config();
+    // Set counter wrap value to generate PWM interrupt by this value
+    pwm_set_wrap(slice_num, 7997);
+    // Load the configuration into our PWM slice, and set it running.
+    pwm_set_enabled(slice_num, true);
 
     // write message to VRAM
     vram_strings(0, 1, "Hello, world!");
@@ -225,51 +268,25 @@ int main() {
     vram_strings(0, 9, "0123456789+-/*");
     vram_strings(0, 10, "@[]<>!%$#&()\\");
 
+    volatile int countup;
+    int cx = 0, cy = 18;
+    char cc;
+
     while (1) {
-        // vertical synchronization duration
-        if (count >= 3 && count <= 5) {
-            vsync();    // vertical SYNC
-        // upper blank area 
-        } else if (count >= 6 && count < V_BASE) {
-            hsync();    // horizontal SYNC
-            BLACK;
-            sleep_us(48);
-        // VRAM drawing area
-        } else if (count >= V_BASE && count < V_BASE + VRAM_H * CHAR_H) {
-            hsync();
-            // left blank??
-            BLACK;
-            sleep_us(1);    // should be tuned
-            // calculate VRAM y position from scan line number
-            int y = (count - V_BASE) / CHAR_H;
-            // horizontal VRAM drawing
-            for (int x = 0; x < VRAM_W; x++) {
-                // read VRAM
-                int c = vram[x][y];
-                // draw bit from character table (ascii_table)
-                for (int w = 0; w < CHAR_W; w++) {
-                    if ((ascii_table[c][count % CHAR_H] & (1 << w)) != 0) {
-                        GRAY; WHITE;
-                    } else {
-                        BLACK; BLACK;
-                    }
-                }
-            }
-            // right blank??
-            BLACK;
-//            sleep_us(6);    // should be tuned
-            sleep_us(3);    // should be tuned
-        } else {
-            hsync();
-            BLACK;
-            sleep_us(48);
+        if (countup % 200000 == 0) {
+            // flip LED
+            flip_led();
         }
-        // count up scan line 
-        count++;
-        // if scan line reach to max
-        if (count > 262) {
-            count = 1;
+        if (countup % 10000 == 0) {
+            // display bar
+            display_bar(16, '#');
         }
+        if (countup % 100000 == 0) {
+            // measure temprature and display it
+            measure_temp(13);
+        }
+
+        countup++;
     }
 
     return 0;
